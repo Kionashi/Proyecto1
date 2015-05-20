6dbac73da54e503d65c900919099c1729c26f931
Anibal Cardozo <cardozo.anibal@gmail.com>
2015-05-19 23:20:40 -0430

    Added Shell.c

---
+++ Shell.c
@@ -0,0 +1,34 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+void mostrarIndicador(); //como ejecutrar whoiam a travez del codigo 
+
+void leerOrden( char *buffer)
+{
+	printf(indicador);
+	pritnf(ruta);
+	scanf()
+};
+
+void analizarOrden(char * buffer){ //usar strok para separar la instruccion en tokenes igual 
+
+
+};
+
+
+main()
+{
+	char* indicador;
+	char* ruta;
+	while(1){
+		mostrarIndicador();
+		leerOrden()	
+		analizarOrden();
+	}
+}
+
