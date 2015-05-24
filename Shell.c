f94c32316e2d3eb813416d98f3783f12303d71ba
Anibal Cardozo <cardozo.anibal@gmail.com>
2015-05-24 16:00:39 -0430

    creacion de algunas funciones

--- Shell.c
+++ Shell.c
@@ -6,29 +6,57 @@
 #include <string.h>
 #include <math.h>
 
-void mostrarIndicador(); //como ejecutrar whoiam a travez del codigo 
 
-void leerOrden( char *buffer)
+void mostrarIndicador(char * indicador, char* ruta) //como ejecutrar whoiam a travez del codigo 
 {
-	printf(indicador);
-	pritnf(ruta);
-	scanf()
+	//printf("imprimiendo indicador\n");
+	printf("%s",indicador);
+	//printf("imprimiendo ruta\n");
+	printf("%s",ruta);
 };
 
-void analizarOrden(char * buffer){ //usar strok para separar la instruccion en tokenes igual 
+char * leerOrden(char* orden, char*indicador, char*ruta)
+{
+	//printf("AUN NO ENTRO\n");
 
+		//printf("ENTRE\n");
+		mostrarIndicador(indicador,ruta);
+		scanf("%s",orden);
+		return orden;		
+
+
+};
+
+void analizarOrden(char * str) //usar strok para separar la instruccion en tokenes igual 
+{ 
+	printf("analizando orden: %s\n",str);
+
+ 	char * pch = (char*)calloc(100,sizeof(char));
+  	pch = strtok (str," ");
+  	while (pch != NULL)
+ 	{
+    	printf ("%s\n",pch);
+    	pch = strtok (NULL, " ");
+  	}
 
 };
 
 
 main()
 {
-	char* indicador;
-	char* ruta;
-	while(1){
-		mostrarIndicador();
-		leerOrden()	
-		analizarOrden();
-	}
+	char* indicador = (char*)calloc(100,sizeof(char));
+	char* ruta = (char*)calloc(100,sizeof(char));
+	char* orden = (char*)calloc(200,sizeof(char));
+	indicador = "$NombreDeLaMaquina/";
+	ruta="RutaActual/";
+	//while(1){
+
+//  	*******LEER ORDEN*******
+		orden = leerOrden(orden, indicador, ruta);
+		printf("orden leida: %s\n",orden);	
+//		*******ANALIZAR ORDEN*****
+
+		//analizarOrden(orden);
+	//}
 }
 
